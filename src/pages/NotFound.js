4a7e3c3af06d3c680abd4af2023199f3ce1b0173
Yago Arroyo <yagoag@gmail.com>
2020-07-31 01:41:11 -0300

    Improved 404 message

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const NotFound = () => <div>Nothing to see here.</div>;
+const Message = styled.div`
+  font-size: 16px;
+  font-weight: 600;
+  color: #fefefe;
+`;
+
+const NotFound = () => (
+  <Message>
+    Looks like the Pokeball you found is empty... Try going back to the{' '}
+    <Link to="/">main page</Link>.
+  </Message>
+);
 
 export default NotFound;
